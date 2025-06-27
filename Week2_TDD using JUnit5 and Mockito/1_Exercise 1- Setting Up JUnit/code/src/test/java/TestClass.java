b0d1ff410b681a332f8e57c27ba958aa35c8a436
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-06-27 20:27:49 +0530

    Added Week 2 TDD Ex 1

---
+++ Week2_TDD using JUnit5 and Mockito/1_Exercise 1- Setting Up JUnit/code/src/test/java/TestClass.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestClass {
+    @Test
+    public void additionTest(){
+        assertEquals(2+3,5);
+    }
+}
