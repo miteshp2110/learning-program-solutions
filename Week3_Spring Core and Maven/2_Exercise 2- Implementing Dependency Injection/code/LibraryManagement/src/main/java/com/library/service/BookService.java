cc0f89532b973e107ffab0175ecf93879d6990cb
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-07-04 16:39:33 +0530

    completed week 3 ex 2

--- Week3_Spring Core and Maven/2_Exercise 2- Implementing Dependency Injection/code/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week3_Spring Core and Maven/2_Exercise 2- Implementing Dependency Injection/code/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -3,6 +3,7 @@ package com.library.service;
 import com.library.repository.BookRepository;
 
 public class BookService {
+
     private BookRepository bookRepository;
 
     public void setBookRepository(BookRepository bookRepository) {
